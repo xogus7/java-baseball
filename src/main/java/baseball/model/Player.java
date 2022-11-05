8e2042b0ca9268c65939c489179f905e832123bf
xogus7 <xogus2676@gmail.com>
2022-11-05 21:36:08 +0900

    feat: add Computer, Player

---
+++ src/main/java/baseball/model/Player.java
@@ -0,0 +1,13 @@
+package baseball.model;
+
+import java.util.List;
+
+public class Player {
+    private List<Integer> numbers;
+    public Player(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+}
